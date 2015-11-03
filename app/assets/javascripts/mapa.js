6d9e491aa018e0d9a82da205ac6e36b3eeaf90cd
Facundo <rodrif89@gmail.com>
2015-11-02 22:21:32 -0300

    rightclick on map, create new person

--- app/assets/javascripts/mapa.js
+++ app/assets/javascripts/mapa.js
@@ -4,14 +4,11 @@ var mapProp;
 var infowindow;
 
 function loadIndexAction() {
-
   listaPersonasViewModels = $('#personas').data('personas');
-
   loadMapaScript('initialize');
 }
 
 function loadMapaScript(calllback) {
-
   var script = document.createElement("script");
   script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDtDVYB3gnF3bj6nMqzma3IHMNUvYe_rdY&callback=" + calllback;
 
@@ -19,30 +16,31 @@ function loadMapaScript(calllback) {
 }
 
 function initialize() {
-
   mapProp = {
     center:new google.maps.LatLng(-34.6425867,-58.5659176),
     zoom:15,
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
   
-  map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  map.addListener('rightclick', function() { //TODO no anda
+    window.location = '/people/new';
+  });
+
+console.log(map);
+  console.log("fin");
 
   addPersonasToMap();
 }
 
-function addPersonasToMap() {
- 
+function addPersonasToMap() { 
   for (var i = 0; i < listaPersonasViewModels.length; i++) {
-
     addMarker(listaPersonasViewModels[i]);
   }
-
   infowindow = new google.maps.InfoWindow();
  }
 
 function addMarker(personaViewModel) {
-
   var latlng = new google.maps.LatLng(personaViewModel.latitud, personaViewModel.longitud);
 
   var marker = new google.maps.Marker({
@@ -58,7 +56,6 @@ function addMarker(personaViewModel) {
     //map.setZoom(18); 
     mostrarInfoWindow(event, marker); 
   });
-
   google.maps.event.addListener(marker, 'click', function() { 
     window.location = '/people/' + marker.data.persona_id + '/edit';
   });
