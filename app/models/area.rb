cbb2bb937a3b077dcbf598575f684a4afba67034
Facundo <rodrif89@gmail.com>
2015-11-30 22:13:06 -0300

    area y zona no repetidos

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,7 +1,7 @@
 class Area < ActiveRecord::Base
-  validates :nombre, presence: true,
+  validates :nombre, presence: true, uniqueness: { case_sensitive: false },
     format: { with: /\A[a-zA-Z\sáéíóúÁÉÍÓÚ]+\z/, message: I18n.t('common.errores.solo_letras') }
-  
+ 
   self.per_page = 20
 
   filterrific(
