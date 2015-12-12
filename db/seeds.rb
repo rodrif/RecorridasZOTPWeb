4cb50547835c54518d787ef8cb9b646983852e81
Facundo <rodrif89@gmail.com>
2015-12-12 13:49:32 -0300

    agrego test del link Visitas, verificando q se cargue la ultima ubicacion de la persona

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,16 +42,19 @@ personas << Person.create!(
 	nombre: "Gonzalo", apellido: "Rodriguez",
 	zone: zonas[0], ranchada: ranchadas[1], familia: familias[0], state_id: 1)
 
+personas << Person.create!(
+	nombre: "Laura", apellido: "Aquino",
+	zone: zonas[2], state_id: 1)
+
 10.times do |i|
   personas << Person.create!(nombre: Faker::Name.first_name, apellido: Faker::Name.last_name.gsub(/[^a-zA-Z]/,''), zone: zonas[i%4], state_id: 1)  
   Rails.logger.debug "#{personas[i].to_yaml}"
 end
 
-
 visits = Visit.create!([ { descripcion: 'Descripcion1', person: personas[0], fecha: Time.now,
  latitud: '-34.6425867', longitud: '-58.5659176', state_id: 1 } ])
 
-2.upto(10) do |i|
+2.upto(12) do |i|
   visits << Visit.create!(descripcion: 'Descripcion1', person: personas[i], fecha: Time.now.ago(2.days),
   	latitud: Faker::Address.latitude, longitud: Faker::Address.longitude, state_id: 1)
 
--- spec/features/visita_spec.rb
+++ spec/features/visita_spec.rb
@@ -7,10 +7,19 @@ describe 'Visitas' do
 
   subject { page }
 
-  describe "index page" do    
+  describe "index page", js: true do
+    let(:visita) { Person.find_by_nombre("Facundo").visits.first }
     before { visit visits_path }
 
-    it { should have_content('Visitas') }
+    it "borrar visita" do
+      should have_content('Visitas')
+
+      accept_alert do
+        should have_xpath("//tr[contains(., 'Facundo')]", count: 1)
+        find(:xpath, "//tr[contains(., 'Facundo')]/td/a", :text => 'Borrar').click
+      end
+      should have_xpath("//tr[contains(., 'Facundo')]", count: 0)
+    end
   end
   
   describe "edit page" do   
@@ -21,6 +30,24 @@ describe 'Visitas' do
       should have_field('visit[descripcion]', with: 'Descripcion1')
       should have_field('visit[latitud]', with: '-34.6425867')
       should have_field('visit[longitud]', with: "-58.5659176")
+
+      fecha = 1.day.from_now
+      fill_in 'visit[fecha]', with: fecha
+      fill_in 'visit[descripcion]', with: "comentario de prueba edit page visita"
+      fill_in 'visit[latitud]', with: "-34.642567854"
+      fill_in 'visit[longitud]', with: "-58.565912323"
+
+      find('input[name="commit"]').click
+      expect(current_path).to eq(visits_path)
+      should have_content('Visita actualizada correctamente.')
+
+      should have_xpath("//tr[contains(., 'Facundo')]", text: /Facundo.*comentario de prueba edit page visita/)
+
+      visita.reload
+      expect(visita.fecha.to_s).to eq(fecha.to_s)
+      expect(visita.descripcion).to eq("comentario de prueba edit page visita")
+      expect(visita.latitud).to eq(-34.642567854)
+      expect(visita.longitud).to eq(-58.565912323)
     end
   end
 
@@ -47,6 +74,24 @@ describe 'Visitas' do
       expect(nuevaVisita.descripcion).to eq("comentario de prueba new page visita")
       expect(nuevaVisita.latitud).to eq(-34.6425678)
       expect(nuevaVisita.longitud).to eq(-58.5659123)
+    end
+  end
+
+  describe "visits link", js: true do
+    let!(:persona) { Person.find_by_nombre("Laura") }
+    let!(:visita) { persona.visits.activas.first }
+    before { visit people_path }
+
+    it "nueva visita con ultima ubicacion de persona" do
+      find(:xpath, "//tr[contains(., 'Laura')]/td/a", :text => 'Visitas').click
+
+      assert_current_path(visits_path + "?person_id=#{persona.id}")
+
+      click_link('Nueva visita')
+
+      should have_select('visit[person_id]', selected: "Laura Aquino")
+      should have_field('visit[latitud]', with: visita.latitud)
+      should have_field('visit[longitud]', with: visita.longitud)
     end
   end
 
