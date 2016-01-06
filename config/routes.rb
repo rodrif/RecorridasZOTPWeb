d53f7da1a2be19898494ebec8b313ad6c96a27ca
Facundo <rodrif89@gmail.com>
2016-01-05 22:52:10 -0300

    route with post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do
-     get 'areas/download' => 'areas#download'
+    post 'areas/download' => 'areas#download'
   end
 
   resources :referentes
