c82b2131869dce1b41da3774c47cf841aff1a0ba
Facundo <rodrif89@gmail.com>
2016-01-09 12:36:20 -0300

    fix api controller forgery

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -3,6 +3,7 @@ class Api::ApiController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   #protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json'}
+  #skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json'}
 
 end
