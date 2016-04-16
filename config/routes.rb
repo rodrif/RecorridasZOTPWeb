5b0b2971407c35937ac958fb803f097ce1665713
Facundo <rodrif89@gmail.com>
2016-04-16 10:51:35 -0300

    rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   namespace :api do
-    #mount_devise_token_auth_for 'User', at: 'auth'
+    mount_devise_token_auth_for 'User', at: 'auth'
     post 'areas/download' => 'areas#download'
     post 'areas/upload' => 'areas#upload'
     post 'zones/download' => 'zones#download'
