48cea2223cd5fd69ed857765daccf51ccbac481c
Facundo <rodrif89@gmail.com>
2016-04-21 22:03:26 -0300

    controladores users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    #sessions: 'users/sessions'
+    registrations: 'users/registrations'
+  }
+
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth'
     post 'areas/download' => 'areas#download'
