da81c71aa04d3a49b9ffc14f5104e22ca9e6ecd3
Facundo <rodrif89@gmail.com>
2016-04-28 20:51:18 -0300

    redirecciono a la pantalla de acceso denegado cuando faltan permisos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,49 +14,49 @@ class ApplicationController < ActionController::Base
 
   def puede_crear_persona
     if !current_user.rol || !current_user.rol.puede_crear_persona
-      redirect_to root_path, alert: 'Acceso denegado, faltan permisos'
+      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
     end
   end
 
   def puede_editar_persona
     if !current_user.rol || !current_user.rol.puede_editar_persona
-      redirect_to root_path, alert: 'Acceso denegado, faltan permisos'
+      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
     end
   end
 
   def puede_borrar_persona
     if !current_user.rol || !current_user.rol.puede_borrar_persona
-      redirect_to root_path, alert: 'Acceso denegado, faltan permisos'
+      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
     end
   end
 
   def puede_ver_telefono_persona
     if !current_user.rol || !current_user.rol.puede_ver_telefono_persona
-      redirect_to root_path, alert: 'Acceso denegado, faltan permisos'
+      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
     end
   end
 
   def puede_crear_visita
     if !current_user.rol || !current_user.rol.puede_crear_visita
-      redirect_to root_path, alert: 'Acceso denegado, faltan permisos'
+      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
     end
   end
 
   def puede_editar_visita
     if !current_user.rol || !current_user.rol.puede_editar_visita
-      redirect_to root_path, alert: 'Acceso denegado, faltan permisos'
+      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
     end
   end
 
   def puede_borrar_visita
     if !current_user.rol || !current_user.rol.puede_borrar_visita
-      redirect_to root_path, alert: 'Acceso denegado, faltan permisos'
+      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
     end
   end
 
   def puede_ver_web
     if !current_user.rol || !current_user.rol.puede_ver_web
-      redirect_to root_path, alert: 'Acceso denegado, faltan permisos'
+      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
     end
   end
 
