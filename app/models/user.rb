53993a68a44465cbf113d9efc308b07de5f7f1e1
Facundo <rodrif89@gmail.com>
2016-04-30 11:50:15 -0300

    saco doble validacion

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable #, :validatable por doble validacion
 
   belongs_to :rol
   belongs_to :area
