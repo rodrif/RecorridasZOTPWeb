9c0ee6db64c771f1318407f75b9f423f51b479b8
Facundo <rodrif89@gmail.com>
2016-05-05 21:04:28 -0300

    redirecciono y deslogueo si no puede ver web

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,56 +14,65 @@ class ApplicationController < ActionController::Base
 
   def puede_crear_persona
     if !current_user.rol || !current_user.rol.puede_crear_persona
-      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
+      redireccionar
     end
   end
 
   def puede_editar_persona
     if !current_user.rol || !current_user.rol.puede_editar_persona
-      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
+      redireccionar
     end
   end
 
   def puede_borrar_persona
     if !current_user.rol || !current_user.rol.puede_borrar_persona
-      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
+      redireccionar
     end
   end
 
   def puede_ver_telefono_persona
     if !current_user.rol || !current_user.rol.puede_ver_telefono_persona
-      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
+      redireccionar
     end
   end
 
   def puede_crear_visita
     if !current_user.rol || !current_user.rol.puede_crear_visita
-      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
+      redireccionar
     end
   end
 
   def puede_editar_visita
     if !current_user.rol || !current_user.rol.puede_editar_visita
-      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
+      redireccionar
     end
   end
 
   def puede_borrar_visita
     if !current_user.rol || !current_user.rol.puede_borrar_visita
-      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
+      redireccionar
     end
   end
 
   def puede_ver_web
     if !current_user.rol || !current_user.rol.puede_ver_web
-      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
+      redireccionar
     end
   end
 
   def is_admin
     if !current_user.rol_id || current_user.rol_id != 1
-      redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
+      redireccionar
     end
+  end
+
+  private
+
+  def redireccionar
+    if !current_user.rol.puede_ver_web
+      sign_out current_user
+    end
+    redirect_to acceso_denegado_path, alert: 'Acceso denegado, faltan permisos'
   end
 
 end
