f22098ea7df361af2dbd47b527bc36655e62da04
Facundo <rodrif89@gmail.com>
2016-05-07 09:13:01 -0300

    after_sign_out_path_for

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,10 +16,16 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.for(:sign_in) << :attribute
   # end
+
+  # The path used after sign out
+  def after_sign_out_path_for(resource)
+    #super(resource)
+    new_user_session_path
+  end
 end
