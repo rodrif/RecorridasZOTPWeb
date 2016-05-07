3174420dc03604db3add825e8c5c004aac4c2941
Facundo <rodrif89@gmail.com>
2016-05-06 23:09:24 -0300

    borro metodo show notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
 
-  def showNotice
-    if notice
-      return "<p id=\"notice\" class=\"alert alert-success\">#{ notice }</p>".html_safe
-    end
-    if alert
-      return "<p id=\"alert\" class=\"alert alert-danger\">#{ alert }</p>".html_safe
-    end
-    # if info
-    #   return "<p id=\"alert\" class=\"alert alert-info\">#{ info }</p>".html_safe
-    # end
-    # if defined?(error)
-    #   return "<p id=\"alert\" class=\"alert alert-danger\">#{ error }</p>".html_safe
-    # end
-  end
+  # def showNotice
+  #   if notice
+  #     return "<p id=\"notice\" class=\"alert alert-success\">#{ notice }</p>".html_safe
+  #   end
+  #   if alert
+  #     return "<p id=\"alert\" class=\"alert alert-danger\">#{ alert }</p>".html_safe
+  #   end
+  #   # if info
+  #   #   return "<p id=\"alert\" class=\"alert alert-info\">#{ info }</p>".html_safe
+  #   # end
+  #   # if defined?(error)
+  #   #   return "<p id=\"alert\" class=\"alert alert-danger\">#{ error }</p>".html_safe
+  #   # end
+  # end
 
   def loadDefaultDropdowns entity = nil
     @zonas = Zone.zonas_primer_area
