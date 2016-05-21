baa9995083a8332246dfbe9f076dbe957173bd32
Facundo <rodrif89@gmail.com>
2016-05-20 23:23:07 -0300

    fix auditorio, usuario nuevo no esta logueado

--- app/dataAccess/auditoria_data_access.rb
+++ app/dataAccess/auditoria_data_access.rb
@@ -3,7 +3,7 @@ class AuditoriaDataAccess
 	def self.log user, accion, nombreEntidad, entidad
 		auditoria = Auditoria.new
 		auditoria.fecha = Time.now
-    auditoria.email = user.email
+    auditoria.email = user.email if !user.nil?
 		auditoria.accion = accion
 		auditoria.entidad = nombreEntidad
 		auditoria.entity_id = entidad.id
