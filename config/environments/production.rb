cc74c3b8920af47a893e25c830e93a3040d8fa69
Facundo <rodrif89@gmail.com>
2016-05-28 09:40:34 -0300

    agrego url de prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {:host => "serene-wave-43143-test.herokuapp.com"}
+  #config.action_mailer.default_url_options = {:host => "serene-wave-43143-test.herokuapp.com"}
+  config.action_mailer.default_url_options = {:host => "boiling-harbor-80530-prod.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
