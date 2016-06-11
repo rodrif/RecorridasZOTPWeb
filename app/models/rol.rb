8c9dd350ddd8f782d358e8e2f9af686b6469e3ca
rodrif <rodrif89@gmail.com>
2016-06-11 10:26:24 -0300

    agrego constantes a rol

--- app/models/rol.rb
+++ app/models/rol.rb
@@ -1,4 +1,11 @@
 class Rol < ActiveRecord::Base
+
+	ADMINISTRADOR = 1
+	REFERENTE = 2
+	COORDINADOR = 3
+	VOLUNTARIO = 4
+	INVITADO = 5
+
 	has_many :notificacion_roles
 	has_many :notificaciones, :through => :notificacion_roles
 
