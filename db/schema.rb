03e309c086a9b5af17ddafa0096ef28b8cf5e9a5
rodrif <rodrif89@gmail.com>
2016-06-25 13:05:13 -0300

    fix column name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617191020) do
+ActiveRecord::Schema.define(version: 20160625122402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 20160617191020) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.integer  "state_id"
-    t.datetime "proxEnvio"
+    t.datetime "prox_envio"
     t.boolean  "finalizada"
   end
 
