aca687df8fadc5de254870a6e7b3be3ba5f0085d
rodrif <rodrif89@gmail.com>
2016-09-03 19:30:17 -0300

    informe visitas

--- app/controllers/informes_controller.rb
+++ app/controllers/informes_controller.rb
@@ -63,4 +63,22 @@ class InformesController < ApplicationController
     end
   end
 
+  def visitas
+    @filterrific = initialize_filterrific(
+      Auditoria,
+      params[:filterrific],
+      select_options: {
+        visitas: Area.options_for_select,
+      },
+      :default_filter_params => {fecha_gte: 3.month.ago.to_date.to_s},
+      persistence_id: false
+    ) or return
+    @visitas = @filterrific.find.visitas
+    # Respond to html for initial page load and to js for AJAX filter updates.
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -66,6 +66,7 @@ Rails.application.routes.draw do
   get 'informes/voluntarios' => 'informes#voluntarios'
   get 'informes/personas' => 'informes#personas'
   get 'informes/cumpleanios' => 'informes#cumpleanios'
+  get 'informes/visitas' => 'informes#visitas'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
