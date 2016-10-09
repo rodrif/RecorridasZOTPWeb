786a021a1f839a19c5f33fe9d5c9377132fc6105
rodrif <rodrif89@gmail.com>
2016-10-08 22:37:35 -0300

    agrego modelo meetings, solo para tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :meetings
   get 'areas/lanzarException' => 'areas#lanzarException'
 
   devise_for :users, controllers: {
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925154735) do
+ActiveRecord::Schema.define(version: 20161008233004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,14 @@ ActiveRecord::Schema.define(version: 20160925154735) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "code"
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notificacion_roles", force: :cascade do |t|
