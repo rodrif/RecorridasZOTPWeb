27d2b3a083fc479b7094cf7a3223c6430dd05969
rodrif <rodrif89@gmail.com>
2016-12-24 18:11:29 -0300

    fix area test

--- test/models/area_test.rb
+++ test/models/area_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class AreaTest < ActiveSupport::TestCase
-  test "area con numeros" do
-    area = Person.new(nombre: 'area con numero 1')
-    assert area.invalid?
-  end
+    test "area con numeros" do
+        area = Area.new(nombre: 'area con numero 2')
+        assert area.invalid?
+    end
 end
