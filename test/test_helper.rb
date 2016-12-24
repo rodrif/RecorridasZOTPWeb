b370662d308e12d82d993aa875be49e3cbd2380d
rodrif <rodrif89@gmail.com>
2016-12-24 15:28:04 -0300

    include devise in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,12 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    include Devise::TestHelpers
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # for debug problems
-  #fixtures :alert_types, :familias, :people, :ranchadas, :alerts, :areas, :zones
+    # for debug problems
+    #fixtures :alert_types, :familias, :people, :ranchadas, :alerts, :areas, :zones
 
-  # Add more helper methods to be used by all tests here...
+    # Add more helper methods to be used by all tests here...
 end
