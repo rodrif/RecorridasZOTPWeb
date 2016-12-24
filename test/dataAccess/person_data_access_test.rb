1d904a20f435656060c4d4d3256eee9b7cb2a345
rodrif <rodrif89@gmail.com>
2016-12-24 19:48:49 -0300

    fix person data access test. Todos los unit tests areglados

--- test/dataAccess/person_data_access_test.rb
+++ test/dataAccess/person_data_access_test.rb
@@ -1,26 +1,37 @@
-class PersonDataAccessTest < ActiveSupport::TestCase
-	
-	test "guardarPersonasFromJson test" do    
-	    respuesta = PersonDataAccess.guardarPersonasFromJson '[{"web_id": 1, "android_id":11, "nombre":"GuardarPersonasFromJson1"},{"android_id":2, "nombre":"GuardarPersonasFromJson2"},{"web_id": 3,"android_id":5, "nombre":"personaABorrar", "estado":3}]'
-	
+class PersonDataAccessTest < ActionController::TestCase
+
+	setup do
+		@user = users(:admin)
+		sign_in @user
+	end
+
+	test "upload and download test" do
+		liniers = zones(:liniers)
+		data = [
+			{ web_id: 1, android_id: 11, nombre: 'GuardarPersonasFromJsonUno', web_zone_id: liniers.id },
+			{ android_id: 2, nombre: 'GuardarPersonasFromJsonDos', web_zone_id: liniers.id },
+			{ web_id: 3, android_id: 5, nombre: 'personaABorrar', estado: 3, web_zone_id: liniers.id }
+		].to_json
+
+	    respuesta = PersonDataAccess.upload @user, data
+
 	    assert_not_nil respuesta, "respuesta null"
 
 		assert respuesta["datos"]["11"] > 0, "respuesta incorrecta de servidor"
 		assert respuesta["datos"]["2"] > 0, "respuesta incorrecta de servidor"
 
-	    assert (Person.find_by nombre: "GuardarPersonasFromJson1"), "No se guardo la persona en la bd"
-		assert (Person.find_by nombre: "GuardarPersonasFromJson2"), "No se guardo la persona en la bd"
-		assert_equal 3, Person.find(3).state_id, "fallo borrado logico" 
-	end
+	    assert (Person.find_by nombre: "GuardarPersonasFromJsonUno"), "No se guardo la persona en la bd"
+		assert (Person.find_by nombre: "GuardarPersonasFromJsonDos"), "No se guardo la persona en la bd"
+		assert_equal 3, Person.find(3).state_id, "fallo borrado logico"
 
-	test "getPersonasDesde" do    
-	    personas = PersonDataAccess.getPersonasDesde
+		# download
+		personas = PersonDataAccess.download
 
 	    #TODO mejorar test
-	    assert_equal 5, personas["datos"].count, "cantidad de personas no coincide"
+	    assert_equal 6, personas["datos"].size, "cantidad de personas no coincide"
 
-	    # assert (Person.find_by nombre: "GuardarPersonasFromJson1"), "No se guardo la persona en la bd"
-		# assert (Person.find_by nombre: "GuardarPersonasFromJson2"), "No se guardo la persona en la bd"
+	    assert (Person.find_by nombre: "GuardarPersonasFromJsonUno"), "No se guardo la persona en la bd"
+		assert (Person.find_by nombre: "GuardarPersonasFromJsonDos"), "No se guardo la persona en la bd"
 	end
 
 end
