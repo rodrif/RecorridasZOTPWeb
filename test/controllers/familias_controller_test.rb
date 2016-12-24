f38be4f4690c8ab6f24cd86816289fc60c91f100
rodrif <rodrif89@gmail.com>
2016-12-24 16:03:16 -0300

    fix familias test

--- test/controllers/familias_controller_test.rb
+++ test/controllers/familias_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class FamiliasControllerTest < ActionController::TestCase
   setup do
     @familia = familias(:one)
+    @user = users(:admin)
+    sign_in @user
   end
 
   test "should get index" do
@@ -18,15 +20,16 @@ class FamiliasControllerTest < ActionController::TestCase
 
   test "should create familia" do
     assert_difference('Familia.count') do
-      post :create, familia: { area_id: @familia.area_id, descripcion: @familia.descripcion, nombre: @familia.nombre, ranchada_id: @familia.ranchada_id, zone_id: @familia.zone_id }
+      post :create, familia: {
+        area_id: @familia.area_id,
+        descripcion: @familia.descripcion,
+        nombre: 'nueva familia',
+        ranchada_id: @familia.ranchada_id,
+        zone_id: @familia.zone_id
+      }
     end
 
-    assert_redirected_to familia_path(assigns(:familia))
-  end
-
-  test "should show familia" do
-    get :show, id: @familia
-    assert_response :success
+    assert_redirected_to familias_path
   end
 
   test "should get edit" do
@@ -35,12 +38,18 @@ class FamiliasControllerTest < ActionController::TestCase
   end
 
   test "should update familia" do
-    patch :update, id: @familia, familia: { area_id: @familia.area_id, descripcion: @familia.descripcion, nombre: @familia.nombre, ranchada_id: @familia.ranchada_id, zone_id: @familia.zone_id }
-    assert_redirected_to familia_path(assigns(:familia))
+    patch :update, id: @familia, familia: {
+      area_id: @familia.area_id,
+      descripcion: 'nueva descripcion',
+      nombre: @familia.nombre,
+      ranchada_id: @familia.ranchada_id,
+      zone_id: @familia.zone_id
+    }
+    assert_redirected_to familias_path
   end
 
   test "should destroy familia" do
-    assert_difference('Familia.count', -1) do
+    assert_difference('Familia.activas.count', -1) do
       delete :destroy, id: @familia
     end
 
