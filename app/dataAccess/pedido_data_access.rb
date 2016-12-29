aed888f5cae24402aee19538e46efcb865817c75
rodrif <rodrif89@gmail.com>
2016-12-28 23:13:03 -0300

    controller, data access y model

---
+++ app/dataAccess/pedido_data_access.rb
@@ -0,0 +1,71 @@
+class PedidoDataAccess
+
+	def self.download datosJson = nil, fecha = nil
+		respuesta = Hash.new
+		respuesta['datos'] = Hash.new
+		respuesta['fecha'] = DateTime.now.utc.strftime('%Y-%m-%d %H:%M:%S.%L')
+
+		query = 'id AS web_id, person_id AS web_person_id, fecha, descripcion, state_id AS estado, updated_at'
+    if fecha.nil?
+      respuesta['datos'] = Pedido.select(query)
+    else
+      respuesta['datos'] = Pedido.where('updated_at > ?', fecha).select(query)
+    end
+
+		respuesta['datos'].each_with_index do |p, index|
+			fecha = p.fecha.to_datetime.strftime('%Q')
+			p = p.attributes
+			p['fecha'] = fecha
+			respuesta['datos'][index] = p
+		end
+
+		respuesta
+	end
+
+	def self.upload user, json, fecha = nil
+		respuesta = Hash.new
+		respuesta['datos'] = Hash.new
+    pedidos = ActiveSupport::JSON.decode(json)
+
+    pedidos.each do |p|
+      if (p['web_id'].nil? || p['web_id'] <= 0)
+        pedido = Pedido.new
+        accion = Auditoria::ALTA
+      else
+        pedido = Pedido.find(p['web_id']);
+        accion = Auditoria::MODIFICACION
+      end
+      if !p['estado'].nil? && p['estado'] == 3
+        PedidoDataAccess.borrar_logico pedido, user
+        accion = Auditoria::BAJA
+      else
+        pedido.state_id = 1
+      end
+      pedido.person_id = p['web_person_id']
+      pedido.fecha = Time.at(p['fecha'] / 1000)
+      pedido.descripcion = p['descripcion'] ? p['descripcion'] : nil
+
+      if (pedido.save(validate: false))
+        if accion != Auditoria::BAJA
+          AuditoriaDataAccess.log user, accion, Auditoria::PEDIDO, pedido
+        end
+        respuesta['datos'][p['android_id'].to_s] = pedido.id
+      else
+        respuesta['datos'][p['android_id'].to_s] = -1
+      end
+    end
+
+    respuesta['fecha'] = DateTime.now.utc.strftime('%Y-%m-%d %H:%M:%S.%L')
+    respuesta
+  end
+
+  def self.borrar_logico pedido, user
+    pedido.state_id = 3
+    if user && !pedido.person_id.nil? && pedido.person.state_id != 3
+      AuditoriaDataAccess.log user, Auditoria::BAJA, Auditoria::PEDIDO, pedido
+    end
+    pedido.person_id = nil
+    pedido.save(validate: false)
+  end
+
+end
--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,3 +1,56 @@
 class Pedido < ActiveRecord::Base
-  belongs_to :person
+    belongs_to :person
+
+ self.per_page = 20
+
+  filterrific(
+    available_filters: [
+      :search_query,
+      :with_area_id,
+      :with_zone_id,
+      :with_person_id,
+    ]
+  )
+
+  scope :search_query, lambda { |query|
+    return nil  if query.blank?
+
+    # condition query, parse into individual keywords
+    terms = query.downcase.split(/\s+/)
+
+    # replace "*" with "%" for wildcard searches,
+    # append '%', remove duplicate '%'s
+    terms = terms.map { |e|
+    (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+    }
+    # configure number of OR conditions for provision
+    # of interpolation arguments. Adjust this if you
+    # change the number of OR conditions.
+    num_or_conds = 1
+
+    joins(:person).where(
+    terms.map { |term|
+      "(LOWER(people.nombre) LIKE ?)"
+    }.join(' AND '),
+    *terms.map { |e| [e] * num_or_conds }.flatten
+    )
+  }
+
+  scope :with_area_id, lambda { |area_id|
+    joins(person: [{zone: :area}]).where("areas.id = ?", area_id)
+  }
+
+  scope :with_zone_id, lambda { |zone_id|
+    joins(person: :zone).where("zones.id = ?", zone_id)
+  }
+
+  scope :with_person_id, lambda { |person_id|
+    joins(:person).where("people.id = ?", person_id)
+  }
+
+    scope :activos, -> { where.not(state_id: 3).order(fecha: :desc) }
+
+    def getDescripcionAuditoria
+        return "Pedido: #{person.nombre if !person.nil?} Fecha: #{fecha.to_date()} Descripción: #{descripcion}"
+    end
 end
