e932f6f793e9842bdb5830bb3c339afd360c1317
root <root@rough-drop.com>
2017-11-07 19:51:19 -0300

    Cambio permisos de archivos

--- app/dataAccess/notificacion_data_access.rb
+++ app/dataAccess/notificacion_data_access.rb
@@ -72,7 +72,7 @@ class NotificacionDataAccess
   def self.createRequest url, notificacion, topic, areaId, personaId = nil
     request = Net::HTTP::Post.new(url.path, {'Content-Type' =>'application/json', 'Authorization' => 'key=AIzaSyDdrRhWx2vSJF9VQShaBQ1zFo8IkI67Vcc'})
     request.body = "{
-      \"to\": \"/topics/#{topic + "-dev"}\",
+      \"to\": \"/topics/#{topic}\",
       \"data\": {
         \"titulo\": \"#{notificacion.titulo}\",
         \"subtitulo\": \"#{notificacion.subtitulo}\",
