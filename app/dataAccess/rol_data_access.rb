851a879d5a5afe486093e5eb38a0dc98ce55f316
Gonzalo89 <gonzalorodriguez89@gmail.com>
2019-07-24 21:59:16 -0300

    Agrego permiso de ver_estado al rol referente

--- app/dataAccess/rol_data_access.rb
+++ app/dataAccess/rol_data_access.rb
@@ -75,8 +75,8 @@ class RolDataAccess
   end
 
   def self.puede_ver_estado current_user = nil
-    # admin, coordinador
-    return current_user && (current_user.rol_id == 1 || current_user.rol_id == 3)
+    # admin, referente, coordinador
+    return current_user && (current_user.rol_id == 1 || current_user.rol_id == 2 || current_user.rol_id == 3)
   end
 
   def self.puede_editar_usuarios current_user = nil
