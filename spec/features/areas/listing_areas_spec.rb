d22a76ed2b6db78d3fbc8126527a31bd79730116
Martin Margonari <martinmargonari@gmail.com>
2020-11-14 13:52:52 -0300

    Moved confirm to factory

--- spec/features/areas/listing_areas_spec.rb
+++ spec/features/areas/listing_areas_spec.rb
@@ -3,33 +3,19 @@ require 'rails_helper'
 RSpec.feature "Ver sedes" do
 
   before do
-    @admin = create(:user_admin)
-    @admin.confirm
-    @non_admin = create(:user)
-    @non_admin.confirm
-    @departamento = create(:departamento)
+    @area = create(:area)
   end
 
-  context "siendo administrador" do
-    scenario "ve satisfactoriamente" do
+  context "ve satisfoctariamente" do
+    scenario "si usuario es administrador" do
+      let(:admin) { create(:user_admin) }
       login_as @admin
       visit "/"
 
       click_link "Configuración"
-      click_link "Áreas"
+      click_link "Sedes"
 
-      expect(page).to have_content(@departamento.nombre)
-    end
-  end
-
-  context "sin ser administrador" do
-    scenario "no puede ver" do
-      login_as @non_admin
-      visit "/"
-
-      click_link "Configuración"
-
-      expect(page).not_to have_link("Áreas")
+      expect(page).to have_content(@area.nombre)
     end
   end
 end
