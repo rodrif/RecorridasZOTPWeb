12ec55ee2eb364bbe1696381b4c85c5594742778
Martin Margonari <martinmargonari@gmail.com>
2020-11-21 19:52:30 -0300

    Editing pedidos spec

--- spec/features/pedidos/editing_pedidos_spec.rb
+++ spec/features/pedidos/editing_pedidos_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Listar pedidos" do
+RSpec.feature "Editar pedido" do
 
   before do
     @admin = create(:user_admin)
@@ -10,18 +10,26 @@ RSpec.feature "Listar pedidos" do
   end
 
   let!(:persona) { create(:person) }
-  let!(:pedido) { create(:pedido, person: persona)}
+  let!(:pedido_guardado) { create(:pedido, person: persona)}
+  let(:pedido) { build(:pedido)}
 
   scenario "siendo usuario administrador" do
     login_as @admin
     visit "/"
 
     click_link "Pedidos"
+    find(:xpath, "//tr[contains(., '#{persona.full_name}')]/td/a", :class => "glyphicon-edit").click
+
+    select persona.nombre, from: "Persona"
+    fill_in "Fecha", with: pedido.fecha
+    fill_in "Descripción", with: pedido.descripcion
+    check "Completado"
+    click_button "Aceptar"
 
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]")
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]/td", :text => pedido.fecha.to_date)
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]/td", :text => pedido.descripcion)
-    expect(page).to have_xpath("//tr[contains(., '#{persona.nombre}')]/td", :text => "No")
+    expect(page).to have_xpath("//tr[contains(., '#{persona.nombre}')]/td", :text => "Si")
   end
 
   scenario "siendo usuario coordinador" do
@@ -29,11 +37,18 @@ RSpec.feature "Listar pedidos" do
     visit "/"
 
     click_link "Pedidos"
+    find(:xpath, "//tr[contains(., '#{persona.full_name}')]/td/a", :class => "glyphicon-edit").click
+
+    select persona.nombre, from: "Persona"
+    fill_in "Fecha", with: pedido.fecha
+    fill_in "Descripción", with: pedido.descripcion
+    check "Completado"
+    click_button "Aceptar"
 
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]")
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]/td", :text => pedido.fecha.to_date)
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]/td", :text => pedido.descripcion)
-    expect(page).to have_xpath("//tr[contains(., '#{persona.nombre}')]/td", :text => "No")
+    expect(page).to have_xpath("//tr[contains(., '#{persona.nombre}')]/td", :text => "Si")
   end
 
   scenario "siendo usuario referente" do
@@ -41,11 +56,18 @@ RSpec.feature "Listar pedidos" do
     visit "/"
 
     click_link "Pedidos"
+    find(:xpath, "//tr[contains(., '#{persona.full_name}')]/td/a", :class => "glyphicon-edit").click
+
+    select persona.nombre, from: "Persona"
+    fill_in "Fecha", with: pedido.fecha
+    fill_in "Descripción", with: pedido.descripcion
+    check "Completado"
+    click_button "Aceptar"
 
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]")
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]/td", :text => pedido.fecha.to_date)
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]/td", :text => pedido.descripcion)
-    expect(page).to have_xpath("//tr[contains(., '#{persona.nombre}')]/td", :text => "No")
+    expect(page).to have_xpath("//tr[contains(., '#{persona.nombre}')]/td", :text => "Si")
   end
 
   scenario "siendo usuario voluntario" do
@@ -53,10 +75,17 @@ RSpec.feature "Listar pedidos" do
     visit "/"
 
     click_link "Pedidos"
+    find(:xpath, "//tr[contains(., '#{persona.full_name}')]/td/a", :class => "glyphicon-edit").click
+
+    select persona.nombre, from: "Persona"
+    fill_in "Fecha", with: pedido.fecha
+    fill_in "Descripción", with: pedido.descripcion
+    check "Completado"
+    click_button "Aceptar"
 
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]")
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]/td", :text => pedido.fecha.to_date)
     expect(page).to have_xpath("//tr[contains(., '#{persona.full_name}')]/td", :text => pedido.descripcion)
-    expect(page).to have_xpath("//tr[contains(., '#{persona.nombre}')]/td", :text => "No")
+    expect(page).to have_xpath("//tr[contains(., '#{persona.nombre}')]/td", :text => "Si")
   end
 end
