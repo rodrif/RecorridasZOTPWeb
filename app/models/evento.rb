79fe1b558719fe363336ba141a3adcd1ef581620
Martin Margonari <martinmargonari@gmail.com>
2020-12-07 14:32:08 -0300

    eventos ok

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -2,4 +2,19 @@ class Evento < ActiveRecord::Base
   belongs_to :person
   belongs_to :user
   has_one :notificacion, dependent: :destroy
+
+  validates :titulo, :fecha_desde, :fecha_hasta, presence: true
+  validate :fecha_valida
+
+  private
+
+  def fecha_valida
+    if fecha_desde.past?
+      errors.add(:fecha_desde, "no puede ser anterior a la hora actual")
+    end
+
+    if fecha_hasta < fecha_desde
+      errors.add(:fecha_hasta, "no puede ser anterior a la hora de inicio")
+    end
+  end
 end
--- spec/features/eventos/creating_evento_spec.rb
+++ spec/features/eventos/creating_evento_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Crear evento" do
   let!(:persona) { create(:person) }
   let(:evento) { build(:evento )}
 
-  scenario "siendo administrador" do
+  def fill_form_and_create evento
     login_as admin
     visit "/"
 
@@ -21,8 +21,49 @@ RSpec.feature "Crear evento" do
     select_date_and_time evento.fecha_hasta, from: "evento_fecha_hasta"
     select persona.nombre, from: "Persona"
     fill_in "Ubicación", with: evento.ubicacion
+
     click_button "Aceptar"
+  end
+
+  scenario "satisfactoriamente" do
+    fill_form_and_create evento
 
     expect(page).to have_content("Evento creado correctamente")
+  end
+
+  scenario "al crear crea una notificación asociada, una hora antes para todos los roles del area del user" do
+    fill_form_and_create evento
+
+    expect(page).to have_content("Evento creado correctamente")
+
+    notificacion = Evento.first.notificacion
+    expect(notificacion.titulo).to eq(evento.titulo)
+    expect(notificacion.subtitulo).to eq(persona.full_name)
+    expect(notificacion.areas).to include(admin.area)
+    expect(notificacion.rol_ids).to eq([1,2,3,4])
+    expect(notificacion.prox_envio).to eq(evento.fecha_desde.advance(hours: -1).change(:min => 0))
+  end
+
+  context "falla al crear" do
+    scenario "Titulo vacio falla" do
+      evento.titulo = ""
+      fill_form_and_create evento
+
+      expect(page).to have_content("Título no puede estar en blanco")
+    end
+
+    scenario "fecha desde previa a actual falla" do
+      evento.fecha_desde = DateTime.now.advance(hours: -1)
+      fill_form_and_create evento
+
+      expect(page).to have_content("Fecha desde no puede ser anterior a la hora actual")
+    end
+
+    scenario "fecha hasta previa a fecha desde" do
+      evento.fecha_hasta = evento.fecha_desde.advance(hours: -1)
+      fill_form_and_create evento
+
+      expect(page).to have_content("Fecha hasta no puede ser anterior a la hora de inicio")
+    end
   end
 end
