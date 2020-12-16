3a4f955fb8b5a9ae27c06c258684370cf829873c
mmargonari <martin.margonari@mercadolibre.com>
2020-12-15 22:21:56 -0300

    visit requets

---
+++ spec/requests/visits_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require 'support/shared_examples_requests'
+
+RSpec.describe "People", type: :request do
+
+  let!(:persona) { create(:person) }
+  let!(:visita) { create(:visit, person: persona)}
+
+  let(:voluntario) { create(:user_voluntario)}
+
+  describe 'DELETE /visits/:id' do
+    context 'con user voluntario' do
+      before do
+        login_as voluntario
+        delete"/visits/#{visita.id}"
+      end
+
+      include_examples "access denied"
+    end
+  end
+
+end
