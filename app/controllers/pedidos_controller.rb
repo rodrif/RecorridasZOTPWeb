4f8869006741d50d93f6ddc8d445f50b7ec41057
mmargonari <martin.margonari@mercadolibre.com>
2020-12-15 23:14:10 -0300

    fixed pedidos_controller

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -1,6 +1,5 @@
 class PedidosController < ApplicationController
     before_action :set_pedido, only: [:show, :edit, :update, :destroy]
-    before_action :puede_ver_web
     before_action :puede_crear_visita, only: [:create]
     before_action :puede_editar_visita, only: [:update]
     before_action :puede_borrar_visita, only: [:destroy]
